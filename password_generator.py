d49aa3c40e92831f7fe09cdeeaf12a98bb7d5a5e
GudikandulaPravallika <21bq1a4250@vvit.net>
2023-09-28 21:26:04 +0530

    Password Generator : It generates a random password in a specified length by the user

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,9 @@
 import string
 import random
 length=int(input("enter desired length of pasword"))
-complexity_type=input("enter type of complexity like :\n1.easy\n2.medium\n3.hard")
+complexity_type=input("enter type of complexity like :\n1.easy\n2.hard")
 characters=""
-#while(True):
+
 if(complexity_type == "easy".casefold()):
     characters+=string.ascii_letters+string.digits
 elif(complexity_type == "hard".casefold()):
